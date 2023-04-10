03bb1bfd2e02c4ded0be71d514325ca08bc91336
kennymcmillan <kennymcmillan29@gmail.com>
2023-04-10 15:24:11 +0300

    Update code.py

--- code.py
+++ code.py
@@ -3,9 +3,9 @@ import fitparse
 import pandas as pd
 import streamlit as st
 import base64
-from PIL import Image
 
-image = Image.open('Aspire_Academy_Logo_White.png') 
+from PIL import Image
+#image = Image.open('Aspire_Academy_Logo_White.png')
 
 def parse_fit_file(file):
     # Load the .fit file
@@ -19,19 +19,9 @@ def parse_fit_file(file):
     df['time'] = (df['timestamp'] - start_time).apply(lambda x: x.total_seconds() + 1)
 
     df = df.drop('unknown_87', axis=1)
-    
-    df = df.drop(df.columns[[6, 10, 11,12,13,14]], axis=1)
-    df['left_right_balance'] = pd.to_numeric(df['left_right_balance'], errors='coerce')
-    
-    start_date = df['timestamp'].min().strftime('%d-%m-%Y')
-    st.write(f"Date: {start_date}")
-    time_col = df.pop('time')
-    df.insert(0, 'time', time_col)
-    df['time'] = df['time'].astype(int)
-    df['distance'] = df['distance'].apply(lambda x: round(x, 1))
+    #df = df.drop('timestamp', axis=1)
 
     return df
-
 
 def download_csv(df):
     csv = df.to_csv(index=False)
@@ -43,19 +33,28 @@ def download_csv(df):
 def app():
 
     st.set_page_config(page_title='Parse .fit File')
-    st.sidebar.image(image, use_column_width=True)
+   # st.sidebar.image(image, use_column_width=True)
     st.sidebar.title("This is an app for downloading .FIT files as a csv file")
     file = st.sidebar.file_uploader('Upload .fit File', type='.fit')
 
     if file is not None:
         df = parse_fit_file(file)
-        
+        start_date = df['timestamp'].min().strftime('%d-%m-%Y')
+        st.write(f"Date: {start_date}")
+
+        time_col = df.pop('time')
+        df.insert(0, 'time', time_col)
+        df['time'] = df['time'].astype(int)
+        df['distance'] = df['distance'].apply(lambda x: round(x, 1))
+
+        df = df.drop(df.columns[[6, 10, 11,12,13,14]], axis=1)
+        df['left_right_balance'] = pd.to_numeric(df['left_right_balance'], errors='coerce')
+
         st.write(df)
 
         if st.button('Process a CSV file'):
             download_csv(df)
             download_location = os.path.join(os.path.expanduser("~"), "Downloads")
-
 
 # Run the Streamlit app
 if __name__ == '__main__':
